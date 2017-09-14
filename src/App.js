ac4120c90b5aaa6fa029b1991a4cecc0c18439fa
Caroline Hermans <carolinehermans@Carolines-Air.wv.cc.cmu.edu>
2017-09-13 23:43:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.numImages = 4
+    this.state = {
+      currImgNum: Math.floor(Math.random() * this.numImages) + 1
+    }
+  }
+
+  resetCurrImgNum() {
+    this.setState({currImgNum: Math.floor(Math.random() * this.numImages) + 1})
+  }
+
+  render() {
+    return (
+      <div>
+        <img src={`./img/${this.state.currImgNum}.jpg`}/>
+        <div className="button" onClick={() => this.resetCurrImgNum()}>
+          <div className="button-text noselect">
+            Another One
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
